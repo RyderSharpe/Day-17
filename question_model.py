eb5805ade896cc241720bdd7c8a31a13479b1e57
Ryder Sharpe <125460623+RyderSharpe@users.noreply.github.com>
2024-05-07 07:12:27 -0700

    Add files via upload

--- question_model.py
+++ question_model.py
@@ -1,9 +1,12 @@
 # TODO - create a Question class with and __init()__ method with two attributes: text and answer attribute
 class Question:
     def __init__(self, q_text, q_answer):
-        self.text = q_text # attribute
-        self.answer = q_answer # attribute
+        # Set the value of the text attribute to the value of q_text
+        self.text = q_text
+        # Set the value of the answer attribute to the value of q_answer
+        self.answer = q_answer
 
 
-new_q = Question("Question", "answer") # These are passed into the Question class.
-print(new_q.text) # To access the text
+new_q = Question("Question", "Answer") # These are passed into the Question class.
+# print(new_q.text) # To access the text
+# print(new_q.answer)
--- tests.py
+++ tests.py
@@ -1,7 +1,1 @@
-class Car:
-    def __init__(self, seats):
-        self.seats = seats
 
-
-my_car=Car(5)
-print(f"mycar.seats = {my_car.seats}")
